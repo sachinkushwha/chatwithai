5df08353f1812423ace7566836bef1bb4da95d58
sachinkushwha <152092000+sachinkushwha@users.noreply.github.com>
2025-08-04 18:39:49 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 export const Chat=async(usermsg)=>{
     console.log(usermsg);
-const response=await fetch(`http://localhost:3000`,{
+const response=await fetch(`https://chatwithai-seven.vercel.app/`,{
     method:"POST",
     headers:{"Content-Type": "application/json"},
     body:JSON.stringify({message:usermsg})
